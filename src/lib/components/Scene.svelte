b3260320722f41a35c36ab54651f2ea1fab25ff0
aurelienrichard <56389380+aurelienrichard@users.noreply.github.com>
2024-12-30 15:39:57 +0100

    I have a cube

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { T } from '@threlte/core'
-	import { OrbitControls } from '@threlte/extras'
+	import { OrbitControls, interactivity } from '@threlte/extras'
+	import { Spring } from 'svelte/motion'
+
+	interactivity()
+
+	let rotation = new Spring(0)
+
+	const colors = ['#018a46', '#ffe456', '#df2f31', '#f6f9fd', '#2362fd', '#f36726']
 </script>
 
 <!-- Camera -->
@@ -8,8 +15,28 @@
 	<OrbitControls enableDamping />
 </T.PerspectiveCamera>
 
+<!-- Lights -->
+<T.AmbientLight intensity={1} />
+
 <!-- Object -->
-<T.Mesh>
-	<T.BoxGeometry args={[1, 1, 1]} />
-	<T.MeshBasicMaterial color="red" />
+<T.Mesh
+	rotation.y={rotation.current}
+	onclick={() => {
+		rotation.target = rotation.current - Math.PI / 2
+	}}
+>
+	<T.BoxGeometry />
+	{#each colors as color}
+		<T.MeshStandardMaterial
+			{color}
+			attach={({ parent, ref }) => {
+				// @ts-expect-error parent is unknown
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				if (Array.isArray(parent.material)) parent.material = [...parent.material, ref]
+				// @ts-expect-error parent is unknown
+				else parent.material = [ref]
+			}}
+			}}
+		/>
+	{/each}
 </T.Mesh>
